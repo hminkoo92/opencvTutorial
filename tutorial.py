5b8489d437c598efdb0032f40afb347eb97801c6
hminkoo92 <dakku92@me.com>
2015-12-31 14:21:10 +0900

    Update(Delete comment due to error)

--- tutorial.py
+++ tutorial.py
@@ -1,11 +1,11 @@
 # http://docs.opencv.org/3.0-beta/doc/py_tutorials/py_gui/py_video_display/py_video_display.html
 import numpy as np
 import cv2
-# 카메라를 쓸 준비를 한다...
+
 cap = cv2.VideoCapture(0)
 
 while(True):
-    # Capture frame-by-frame / 읽어들인다. ret: 정상작동유무, frame :캡처한 정보를 담고있음
+    # Capture frame-by-frame 
     ret, frame = cap.read()
 
     # Our operations on the frame come here
@@ -16,7 +16,7 @@ while(True):
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# When everything done, release the capture // release는 놓아준다.
+# When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
 
