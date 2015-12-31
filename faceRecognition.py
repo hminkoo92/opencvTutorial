adcf833f65729e3d3652a297c6b6beb6cafcaefa
hminkoo92 <dakku92@me.com>
2015-12-31 17:29:33 +0900

    Face Recognition(실시간으로 얼굴 인식)

--- faceRecognition.py
+++ faceRecognition.py
@@ -1,27 +1,37 @@
-# Face recognition: http://docs.opencv.org/master/d7/d8b/tutorial_py_face_detection.html#gsc.tab=0
+# http://docs.opencv.org/3.0-beta/doc/py_tutorials/py_gui/py_video_display/py_video_display.html
 import numpy as np
 import cv2
 
 face_cascade = cv2.CascadeClassifier('C:\Users\dakku\Downloads\opencv\sources\data\haarcascades\haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('C:\Users\dakku\Downloads\opencv\sources\data\haarcascades\haarcascade_eye.xml')
 
-img = cv2.imread('sachin.jpg')
+cap = cv2.VideoCapture(0)
 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+while(True):
+    # Capture img 
+    ret, img = cap.read()
 
+    # Our operations on the frame here
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    # Check Face
+    for (x,y,w,h) in faces:
+	    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+	    roi_gray = gray[y:y+h, x:x+w]
+	    roi_color = img[y:y+h, x:x+w]  
+	    eyes = eye_cascade.detectMultiScale(roi_gray)
+	    for (ex,ey,ew,eh) in eyes:
+	  		cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+	     
 
-for (x,y,w,h) in faces:
-    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-    roi_gray = gray[y:y+h, x:x+w]
-    roi_color = img[y:y+h, x:x+w]
+    # Display the resulting img
+    cv2.imshow('face',img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-    eyes = eye_cascade.detectMultiScale(roi_gray)
-    for (ex,ey,ew,eh) in eyes:
-  		cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-     
-cv2.imshow('img',img)
-cv2.waitKey(0)
+# When everything done, release the capture
+cap.release()
 cv2.destroyAllWindows()
+
