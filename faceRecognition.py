7fd61a97681dafa16f5a0cf9925190385ac4b2fb
hminkoo92 <dakku92@me.com>
2015-12-31 22:02:45 +0900

    Eye Recognition 기능 삭제

--- faceRecognition.py
+++ faceRecognition.py
@@ -3,7 +3,6 @@ import numpy as np
 import cv2
 
 face_cascade = cv2.CascadeClassifier('C:\Users\dakku\Downloads\opencv\sources\data\haarcascades\haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('C:\Users\dakku\Downloads\opencv\sources\data\haarcascades\haarcascade_eye.xml')
 
 cap = cv2.VideoCapture(0)
 
@@ -20,8 +19,6 @@ while(True):
     for (x,y,w,h) in faces:
 	    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
 	    roi_gray = gray[y:y+h, x:x+w]
-	    #roi_color = img[y:y+h, x:x+w]  
-	    #eyes = eye_cascade.detectMultiScale(roi_gray)
 	     
 
     # Display the resulting img
