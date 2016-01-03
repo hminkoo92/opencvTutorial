9284ab3388b5cb96182f4ef2d75eea29e518703e
hminkoo92 <dakku92@me.com>
2016-01-03 16:39:28 +0900

    Update pedestrianDetection.py(실시간 보행자 인식 성공)

--- pedestrianDetection.py
+++ pedestrianDetection.py
@@ -8,20 +8,19 @@ import imutils
 import cv2
  
 # construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--images", required=True, help="images")
-args = vars(ap.parse_args())
- 
+cap = cv2.VideoCapture(0)
 # initialize the HOG descriptor/person detector
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
 
 # loop over the image paths
-for imagePath in paths.list_images(args["images"]):
+while(True):
+	# read capture image
+	ret, image = cap.read()
+
 	# load the image and resize it to (1) reduce detection time
 	# and (2) improve detection accuracy
-	image = cv2.imread(imagePath)
 	image = imutils.resize(image, width=min(400, image.shape[1]))
 	orig = image.copy()
  
@@ -44,10 +43,11 @@ for imagePath in paths.list_images(args["images"]):
 		cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)
  
 	# show some information on the number of bounding boxes
-	filename = imagePath[imagePath.rfind("/") + 1:]
+	#filename = imagePath[imagePath.rfind("/") + 1:]
 	
 	# show the output images
 	cv2.imshow("After NMS", image)
-	cv2.waitKey(0)
-
-	
+	if(cv2.waitKey(1) & 0xFF == ord('q')):
+		break		
+cap.release()
+cv2.destroyAllWindows()
