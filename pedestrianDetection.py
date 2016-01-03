8b83184ee799d9c16d0d3d597a9170b5bb3a769d
hminkoo92 <dakku92@me.com>
2016-01-03 17:16:37 +0900

    Update pedestrianDetection.py(필요없는 코드 삭제)

--- pedestrianDetection.py
+++ pedestrianDetection.py
@@ -3,7 +3,6 @@ from __future__ import print_function
 from imutils.object_detection import non_max_suppression
 from imutils import paths
 import numpy as np
-import argparse
 import imutils
 import cv2
  
