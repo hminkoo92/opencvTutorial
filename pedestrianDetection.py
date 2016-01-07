07451a8dde1cd33bb14500199ecb18d59a40ff74
hminkoo92 <dakku92@me.com>
2016-01-07 21:40:02 +0900

    Update pedestrianDetection.py (불필요한 코드 삭제)

--- pedestrianDetection.py
+++ pedestrianDetection.py
@@ -1,10 +1,7 @@
 # import the necessary packages
-from __future__ import print_function
 from imutils.object_detection import non_max_suppression
-from imutils import paths
 import numpy as np
-import imutils
-import cv2
+import imutils, cv2
  
 # construct the argument parse and parse the arguments
 cap = cv2.VideoCapture(0)
@@ -12,7 +9,6 @@ cap = cv2.VideoCapture(0)
 # initialize the HOG descriptor/person detector
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
-
 
 # loop over the image paths
 while(True):
@@ -22,15 +18,10 @@ while(True):
 	# load the image and resize it to (1) reduce detection time
 	# and (2) improve detection accuracy
 	image = imutils.resize(image, width=min(400, image.shape[1]))
-	orig = image.copy()
  
 	# detect people in the image
 	(rects, weights) = hog.detectMultiScale(image, winStride=(4, 4),
 		padding=(8, 8), scale=1.05)
- 
-	# draw the original bounding boxes
-	for (x, y, w, h) in rects:
-		cv2.rectangle(orig, (x, y), (x + w, y + h), (0, 0, 255), 2)
  
 	# apply non-maxima suppression to the bounding boxes using a
 	# fairly large overlap threshold to try to maintain overlapping
@@ -43,8 +34,9 @@ while(True):
 		cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)
  	
 	# show the output images
-	cv2.imshow("After NMS", image)
+	cv2.imshow("People", image)
 	if(cv2.waitKey(1) & 0xFF == ord('q')):
-		break		
+		break
+
 cap.release()
 cv2.destroyAllWindows()
